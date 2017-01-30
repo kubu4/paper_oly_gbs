e29e433a9e4fed596d98148fa77c37ed06c81c19
Sam <samuel.j.white@gmail.com>
2017-01-30 09:24:22 -0800

    add split.sh script

---
+++ data/split.sh
@@ -0,0 +1,1 @@
+iTools Fqtools splitpool -InFq1 160123_I132_FCH3YHMBBXX_L4_OYSzenG1AAD96FAAPEI-109_1.fq.gz -InFq2 160123_I132_FCH3YHMBBXX_L4_OYSzenG1AAD96FAAPEI-109_2.fq.gz -Index index.lst -Flag enzyme.txt -MisMatch -OutDir split
